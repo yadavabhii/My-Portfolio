c48bea2d913d42776e932860f1d1d8a9b1f29e36
yadavabhii <abhii.abhishek.yadav@gmail.com>
2024-11-05 21:02:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text-name", {
+    strings: ["Web Developer!", "Front-end Developer!",],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true,
+    cursorChar: "|"
+})
